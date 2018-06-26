5f48421c396f2e213f52420a2361f58f987cc92e
Alec Pierce <alec.m.pierce@gmail.com>
2018-06-26 08:34:15 -0500

    Start of solution five

---
+++ five/rubysolution1.rb
@@ -0,0 +1,19 @@
+# @param {String} s
+# @return {String}
+def longest_palindrome(s)
+
+    backwardsCounter = 0
+    current = Array.new
+
+    # Iterate through array of all characters in string
+    for i in s.chars
+
+        # Check if character has occurred yet
+        if current.include?(i)
+
+            # Check if duplicate is one position away or two
+
+
+        end
+    end
+end
